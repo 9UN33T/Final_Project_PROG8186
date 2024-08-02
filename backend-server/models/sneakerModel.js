2cb6fdad29a97e9bb17ddc6ef03d175a2b1ccc3f
9UN33T <puneetkashyap94@gmail.com>
2024-08-01 22:36:50 -0400

    final commit with all the changes and updates

--- backend-server/models/sneakerModel.js
+++ backend-server/models/sneakerModel.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const sneakerSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true },
-  price: { type: Number, required: true },
   description: { type: String, required: true },
   imageUrl: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
+  countInStock: { type: Number, required: true },
+}, {
+  timestamps: true
 });
 
 const Sneaker = mongoose.model('Sneaker', sneakerSchema);
